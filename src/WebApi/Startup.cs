fa79f8947083c727d0cc4f0d4cd6cb9078bf42d2
Juan Jose Dos Santos <juan.dossantos@bancontinental.com.py>
2021-06-08 20:44:59 +0000

    Updated Startup.cs

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -31,7 +31,7 @@ namespace Continental.API.WebApi
                 .AgregarAutoMapper()
                 .AddControllers()
                 .AgregarFluentValidation(services)
-                .AgregarKeycloak()
+                .AgregarKeycloak();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
