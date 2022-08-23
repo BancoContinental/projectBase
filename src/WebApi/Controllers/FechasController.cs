12bf42922e5a21811b875cca21ed19c94d766e67
Mathias Ariel de Mestral Granada <mathias.demestral@bancontinental.com.py>
2022-08-23 14:04:00 -0400

    Eliminar carpeta v1

--- src/WebApi/Controllers/FechasController.cs
+++ src/WebApi/Controllers/FechasController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Continental.API.WebApi.Controllers.V1;
+namespace Continental.API.WebApi.Controllers;
 
 public class FechasController : BaseApiController
 {
