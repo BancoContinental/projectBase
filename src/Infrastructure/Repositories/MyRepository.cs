e517508491a698e1311918e2daea9879f5bc05d5
Mathias Ariel de Mestral Granada <mathias.demestral@bancontinental.com.py>
2022-09-07 15:10:17 -0400

    Agregar ejemplos en MyRepository.cs

--- src/Infrastructure/Repositories/MyRepository.cs
+++ src/Infrastructure/Repositories/MyRepository.cs
@@ -1,14 +1,35 @@
-﻿using Continental.API.Core.Interfaces;
+﻿using System.Threading.Tasks;
+using Continental.API.Core.Interfaces;
 using Continental.API.Infrastructure.Database;
+using Continental.API.Infrastructure.Database.Helpers;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Continental.API.Infrastructure.Repositories;
 
 public class MyRepository : IMyRepository
 {
     private readonly IDbUnitOfWork _db;
+    private readonly IConnectionStringFactory _connectionStringFactory;
 
-    public MyRepository(IDbUnitOfWork db)
+    public MyRepository(IDbUnitOfWork db, IConnectionStringFactory connectionStringFactory)
     {
-        _db = db;
+        _db                      = db;
+        _connectionStringFactory = connectionStringFactory;
+    }
+
+    public async Task GetConEntityFramework()
+    {
+        await _db.ActiveDbContext.CuentasCorrientes.ToListAsync();
+    }
+
+    public async Task GetConDapper()
+    {
+        var connectionString = _connectionStringFactory.GetConnectionString(TipoConexion.Active);
+
+        using var connection = new OracleConnection(connectionString);
+
+        await connection.ExecuteScalarAsync<string>("select 'Hola, mundo!' from dual");
     }
 }
