35e3a8cc6919b40b357872b597c53f43fcbb7591
Mathias Ariel de Mestral Granada <mathias.demestral@bancontinental.com.py>
2022-09-30 09:26:58 -0400

    Agregar tests para endpoints

---
+++ tests/ApiBanking.Tests/ApiFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Continental.API.WebApi.Controllers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace ApiBanking.Tests;
+
+public class ApiFactory : WebApplicationFactory<BaseApiController>, IAsyncLifetime
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+        });
+
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(IHostedService));
+        });
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public new Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
